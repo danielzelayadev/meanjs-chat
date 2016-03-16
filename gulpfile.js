59bfdf023db4ede142bba0ce20a2d89172f06e8a
wupa9 <wupaz9@gmail.com>
2016-03-16 15:22:02 -0600

    Add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let gulp     = require('gulp'),
+	babel    = require('gulp-babel'),
+	clean    = require('gulp-clean'),
+	concat   = require('gulp-concat'),
+	copy     = require('gulp-copy'),
+	csscomb  = require('gulp-csscomb'),
+	csso     = require('gulp-csso'),
+	htmlhint = require('gulp-htmlhint'),
+	htmlmin  = require('gulp-htmlmin'),
+	imagemin = require('gulp-imagemin'),
+	server   = require('gulp-live-server'),
+	sass     = require('gulp-sass'),
+	debug    = require('gulp-strip-debug'),
+	uglify   = require('gulp-uglify'),
+	uncss    = require('gulp-uncss'),
+	utl      = require('gulp-util');
