2e1a469ec33b673d71b91b006ec53e3c95fc8825
wupa9 <wupaz9@gmail.com>
2016-03-23 02:21:49 -0600

    Message styles and functionality

--- dev/app/components/home/home.controller.js
+++ dev/app/components/home/home.controller.js
@@ -1,5 +1,26 @@
-export default class HomeController {
-	constructor () {
-		
+let scope = {};
+let me = { username: 'Daniel ZY' };
+
+class HomeController {
+	constructor ($scope) {
+		this.messages = [];
+		scope = $scope;
+	}
+
+	sendMessage (message) {
+		this.messages.push({
+			content: message,
+			sender: { username: me.username }
+		});
+
+		scope.message = "";
+	}
+
+	messageNotMine (message) {
+		return message.sender.username === me.username;
 	}
 }
+
+HomeController.$inject = [ '$scope' ];
+
+export default HomeController;
