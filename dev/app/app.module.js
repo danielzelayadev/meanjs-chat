2129e355b5e49c9ac955d7b4be57ab1d4c58a480
wupa9 <wupaz9@gmail.com>
2016-04-02 15:39:22 -0600

    Set page title

--- dev/app/app.module.js
+++ dev/app/app.module.js
@@ -3,6 +3,8 @@ import * as routes from "angular-ui-router";
 import * as controllers from "./controllers.module";
 import * as directives from "./directives.module";
 import { config } from "./config";
+import { run } from "./run";
 
 angular.module('MEANChat', ['ui.router', controllers.moduleName, directives.moduleName ])
-	.config(config);
+	.config(config)
+	.run(run);
---
+++ dev/app/run.js
@@ -0,0 +1,7 @@
+let run = (rootScope) => {
+	rootScope.title = "MEANChat | Welcome to MEANChat!";
+};
+
+run.$inject = [ '$rootScope' ];
+
+export { run };
